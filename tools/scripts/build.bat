82f2172792ceaa70c4a89e5df9a223a554206e8f
JustinLye <justinmlye@gmail.com>
2022-01-15 16:47:16 -0600

    add vcvars.bat

--- tools/scripts/build.bat
+++ tools/scripts/build.bat
@@ -2,6 +2,6 @@
 setlocal
 call "%~dp0setvars.bat" %~dp0..\..
 call "%~dp0vcvars.bat"
-cmake -S "%SOURCE_DIR%" -B "%BUILD_DIR%" -G "Visual Studio 16 2019" --install-prefix "%INSTALL_DIR%"
-msbuild "%BUILD_DIR%"
+cmake -S "%SOURCE_DIR%" -B "%BUILD_DIR%" -G "Visual Studio 16 2019" --install-prefix "%INSTALL_DIR%" -DCMAKE_BUILD_TYPE=Release
+msbuild "%BUILD_DIR%\sd.sln"
 cmake --install "%BUILD_DIR%"
